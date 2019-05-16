f934f613283d466141dbb5bb8f537e7613127271
Zili Xiang <zx77@cornell.edu>
2019-05-15 22:52:52 -0500

    1028 ac

--- src/sol/q1028.rs
+++ src/sol/q1028.rs
@@ -10,16 +10,21 @@ pub struct Solution {
 
 impl Solution {
     pub fn recover_from_preorder(s: String) -> Option<Rc<RefCell<TreeNode>>> {
-        let (node, dep, remain) = Solution::parse_next(&s);
+        let (node, dep, mut remain) = Solution::parse_next(&s);
 
         let mut stack : Vec<Rc<RefCell<TreeNode>>> = vec![];
         stack.push(Rc::clone(node.as_ref().unwrap()));
 
         while remain.len() > 0 { // the stack size == current depth
-            let (node2, dep2, remain) = Solution::parse_next(remain);
+            let (node2, dep2, remain2) = Solution::parse_next(remain);
+            remain = remain2;
             match node2 {
                 None => break,
                 Some(n) => {
+                    if dep2 == 0 {
+
+                    }
+
                     while stack.len() > dep2 {
                         stack.pop();
                     }
@@ -37,7 +42,6 @@ impl Solution {
         }
 
         node
-
     }
 
     fn peek(v: &Vec<Rc<RefCell<TreeNode>>>) -> Rc<RefCell<TreeNode>> {
@@ -47,35 +51,29 @@ impl Solution {
 
     /// https://doc.rust-lang.org/1.8.0/book/lifetimes.html the lifetime link to read
     fn parse_next(s: &str) -> (Option<Rc<RefCell<TreeNode>>>, usize, &str) {
-        let mut start = 0;
-        while s.chars().nth(start) != Some('-') {
-            start += 1;
+        let mut dep = 0;
+        while dep < s.len() && s.chars().nth(dep) == Some('-') {
+            dep += 1;
+        }
+        let mut end = dep;
+        while end < s.len() && s.chars().nth(end) != Some('-') {
+            end += 1;
         }
 
-        if start == 0 {
-            return (None, 0, s);
-        }
-
-        let num = s[0..start].parse::<i32>().unwrap();
+        let num = s[dep..end].parse::<i32>().unwrap();
         let node = Some(Rc::new(RefCell::new(TreeNode::new(num))));
-        let mut ctr = start;
-        while s.chars().nth(ctr) == Some('-') {
-            ctr += 1;
-        }
-        let j = &s[ctr..];
 
-        (node, ctr - start, j)
+        (node, dep, &s[end..])
 
     }
-
 
 }
 
 #[cfg(test)]
-mod q1028_test {
+pub mod q1028_test {
     use super::*;
     #[test]
-    pub fn test() {
+    pub fn test_run() {
         for (k, v) in self::get_data() {
             let clone = String::clone(&k);
             assert_eq!(Solution::recover_from_preorder(k).unwrap(), Rc::new(RefCell::new(v)));
@@ -96,4 +94,8 @@ mod q1028_test {
         data.insert(input2, res2);
         data
     }
+
+//    pub fn complex_tree() -> TreeNode {
+//
+//    }
 }
